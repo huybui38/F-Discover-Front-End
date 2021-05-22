e6154929f562383af1d769667210ee1e4d08de45
Ngoc Huy <huybui479@gmail.com>
2021-05-22 23:50:52 +0700

    add eslint & prettierrc

---
+++ .eslintrc.js
@@ -0,0 +1,29 @@
+module.exports = {
+    env: {
+        browser: true,
+        es2021: true,
+        node: true,
+    },
+    extends: [
+        'eslint:recommended',
+        'plugin:import/recommended',
+        'plugin:react/recommended',
+        'plugin:react-hooks/recommended',
+        'prettier',
+    ],
+    parserOptions: {
+        ecmaFeatures: {
+            jsx: true,
+        },
+        ecmaVersion: 12,
+        sourceType: 'module',
+    },
+    plugins: ['unused-imports', 'prettier'],
+    rules: {
+        'prettier/prettier': 'error',
+        'no-unused-vars': 'off',
+        'unused-imports/no-unused-imports': 'error',
+        'unused-imports/no-unused-vars': 'warn',
+        'react/react-in-jsx-scope': 'off',
+    },
+}
