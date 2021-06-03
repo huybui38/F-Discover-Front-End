6f466a85c8194d765568d07f419ddcb62c11b0b1
Ngoc Huy <huybui479@gmail.com>
2021-06-03 09:03:19 +0700

    Turn off rule eslint

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
+import { useState } from 'react'
+
 import './App.css'
-import logo from './logo.svg'
+import { signInWithGoogleRedirect, auth } from './services/firebase'
 
 function App() {
+    const onClick = (e) => {
+        signInWithGoogleRedirect()
+    }
+    let [tok, set] = useState('')
+    auth.getRedirectResult()
+        .then((result) => {
+            if (result.credential) {
+                /** @type {firebase.auth.OAuthCredential} */
+                var credential = result.credential
+
+                // This gives you a Google Access Token. You can use it to access the Google API.
+                var token = credential.accessToken
+                set(token)
+                // ...
+            }
+            // The signed-in user info.
+            var user = result.user
+        })
+        .catch((error) => {
+            // Handle Errors here.
+            var errorCode = error.code
+            var errorMessage = error.message
+            // The email of the user's account used.
+            var email = error.email
+            // The firebase.auth.AuthCredential type that was used.
+            var credential = error.credential
+            // ...
+        })
+
     return (
         <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                    Edit <code>src/App.js</code> and save to reload.
-                </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
-            </header>
+            <button onClick={onClick}> Login with google</button>
+            {tok}
         </div>
     )
 }
---
+++ src/configurations/firebase.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+    apiKey: 'AIzaSyDgc54glN2diPUF3gYEDqwTUiw2v8x0nBk',
+    authDomain: 'fap-project-ce70c.firebaseapp.com',
+    databaseURL: 'https://fap-project-ce70c.firebaseio.com',
+    projectId: 'fap-project-ce70c',
+    storageBucket: 'fap-project-ce70c.appspot.com',
+    messagingSenderId: '122493379410',
+    appId: '1:122493379410:web:5037af6b55317b7a5b108b',
+}
