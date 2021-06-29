8f8c73841eb8bfa8b1f64eee789cf9d24cd2fdc9
Ngoc Huy <huybui479@gmail.com>
2021-06-29 23:41:47 +0700

    test tiep

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -28,3 +28,17 @@ export const TestButton = styled.button`
         /* transform: scale(0.9); */
     }
 `
+export const TestButton2 = styled.button`
+    width: ${(props) => (props.fullWidth ? '100%' : 'auto')};
+    cursor: pointer;
+    background: white;
+    border: 1px solid #dee0e1;
+    display: flex;
+    justify-content: ${(props) => (props.center ? 'center' : 'unset')};
+    align-items: center;
+    padding: ${(props) => props.padding || '3px 3px'};
+    &:hover {
+        background-color: rgba(0, 0, 0, 0.03);
+        /* transform: scale(0.9); */
+    }
+`
