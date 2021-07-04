c4f86b36619143822b67a07afa2310f1fb812fad
LeXuanDai371 <65268504+LeXuanDai0307@users.noreply.github.com>
2021-07-04 21:12:30 +0700

    add scroll explore page

--- src/features/Explore/pages/ExplorePage.js
+++ src/features/Explore/pages/ExplorePage.js
@@ -1,10 +1,12 @@
-import React from 'react'
+import React, { useRef, useState } from 'react'
 
 import { VideoFeedItem } from '../components/VideoFeedItem'
 
 export const ExplorePage = () => {
     return (
         <div>
+            <VideoFeedItem />
+            <VideoFeedItem />
             <VideoFeedItem />
             <VideoFeedItem />
             <VideoFeedItem />
