7568abcb52c135a32cbb61bcd9bb6461a49ae36c
LeXuanDai371 <65268504+LeXuanDai0307@users.noreply.github.com>
2021-07-04 21:12:30 +0700

    add scroll explore page

--- src/features/Explore/components/LayoutExplore/index.js
+++ src/features/Explore/components/LayoutExplore/index.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useRef, useState } from 'react'
 
 import { Route, Switch, Redirect } from 'react-router'
 
@@ -10,6 +10,22 @@ import { Sidebar } from '../Sidebar'
 import * as Styled from './styled.elements'
 
 export const LayoutExplore = () => {
+    const prevScrollY = useRef(0)
+    const [goingUp, setGoingUp] = useState(false)
+    const [left, setLeft] = useState(0)
+    const [right, setRight] = useState(4)
+    const onScroll = (e) => {
+        const currentScrollY = e.target.scrollTop
+        if (prevScrollY.current < currentScrollY && goingUp) {
+            setGoingUp(false)
+        }
+        if (prevScrollY.current > currentScrollY && !goingUp) {
+            setGoingUp(true)
+        }
+        prevScrollY.current = currentScrollY
+        console.log('scroll: ', goingUp, currentScrollY)
+    }
+
     return (
         <Styled.Container>
             <Navbar />
@@ -17,7 +33,7 @@ export const LayoutExplore = () => {
             <Styled.SidebarWrapper>
                 <Sidebar />
             </Styled.SidebarWrapper>
-            <Styled.FlexWrapper>
+            <Styled.FlexWrapper onScroll={onScroll}>
                 <Styled.MainWrapper>
                     <Switch>
                         <Route exact strict path="/explore/for-you" component={Explore} />
