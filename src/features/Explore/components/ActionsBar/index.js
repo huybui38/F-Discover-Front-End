f970589182e46c1e9fdbcaf77dbfb26dc33dc2f8
LeXuanDai371 <65268504+LeXuanDai0307@users.noreply.github.com>
2021-07-20 17:16:37 +0700

    fix following page

--- src/features/Explore/components/ActionsBar/index.js
+++ src/features/Explore/components/ActionsBar/index.js
@@ -126,7 +126,7 @@ export const ActionsBar = ({
 
                 {/* Like */}
                 <Styled.ActionItem>
-                    {isLikePost ? (
+                    {isLikePost && isLikePost !== -1 ? (
                         <ButtonIcon
                             onClick={() => handleLikePost()}
                             bgrColor="rgba(255,63,52,0.1)"
