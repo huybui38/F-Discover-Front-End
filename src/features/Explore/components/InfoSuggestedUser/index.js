6f9e3e67d6b753986647526ab6a188e1e9a06688
LeXuanDai371 <65268504+LeXuanDai0307@users.noreply.github.com>
2021-07-20 16:38:36 +0700

    fix info suggest user

--- src/features/Explore/components/InfoSuggestedUser/index.js
+++ src/features/Explore/components/InfoSuggestedUser/index.js
@@ -97,10 +97,10 @@ export const InfoSuggestedUser = ({ user }) => {
             </Name>
             <Interactive>
                 <span>
-                    <b>100</b> Follower
+                    <b>{user.following}</b> Following
                 </span>
                 <span>
-                    <b>200</b> Like
+                    <b>{user.followers}</b> Followers
                 </span>
             </Interactive>
         </InfoWrapper>
